729f3d338a473e9d22654bfb57091261f3496ae9
Matthew <faceincake@gmail.com>
2022-08-09 02:36:54 -0400

    Stable search and example items

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,7 @@ function Search (props) {
   const { state } = useLocation();
   const [loading, setLoading] = useState(true);
   const [itemList, setItemList] = useState([]);
+  const [maxPerPage, setMaxPerPage] = useState(50);
 
   useEffect(() => {
     if (state === null) return;
@@ -29,7 +30,7 @@ function Search (props) {
   function ResultCard (props) {
     const { item } = props;
     return (
-      <div className='m-0 p-3 col-lg-4'>
+      <div className='ResultCard m-0 p-3 col-sm-6 col-md-4 col-lg-3'>
         <Card>
           <CardImg top alt={item.caption} src={item.imageurls.split(';',1)[0]} />
           <CardBody>
@@ -46,7 +47,7 @@ function Search (props) {
     return (
       <div className='SearchResults'>
         <h3>Search results for '{state.query??"ERROR"}'</h3>
-        <div className='d-flex'>
+        <div className='SearchResultCards row row-cols-md-3 row-cols-lg-4'>
           {itemList.map(item => (
             <ResultCard key={item.id} item={item} />
           ))}
